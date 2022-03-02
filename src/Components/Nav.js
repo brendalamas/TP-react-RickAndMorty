31b9cc0ba0cbc6e0652ef4b0c78d9fee865e06cf
brenda <lamasbrenda@hotmail.com>
2022-03-01 22:47:17 -0300

    arreglo

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,5 +1,4 @@
 import { Link } from 'react-router-dom';
-import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
